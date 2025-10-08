8a14e1872ccf2af258615a996afba1a31b681999
Rafael Francisco <5050440+rafaelfrancisco-dev@users.noreply.github.com>
2025-10-08 14:05:13 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
+  nitro: {
+    experimental: {
+      openAPI: true
+    }
+  },
   future: { compatibilityVersion: 4 },
   modules: [
     '@nuxthub/core',
